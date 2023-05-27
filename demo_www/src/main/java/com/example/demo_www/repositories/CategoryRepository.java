6ac290d753b9f34b6e366b3e63c9943c8d0147bc
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2023-05-27 08:12:43 +0700

    done

--- demo_www/src/main/java/com/example/demo_www/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo_www.repositories;
-
-import com.example.demo_www.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
--- demo_www/src/main/java/com/example/demo_www/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo_www.repositories;
-
-import com.example.demo_www.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
